d521fc67c63ca74a7f601184e900da4ef315b785
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-03-02 00:33:26 -0800

    jasonwebtoken added

--- controllers/UserControler.js
+++ controllers/UserControler.js
@@ -1,4 +1,9 @@
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
   res.json({ message: "login successfully" });
@@ -9,7 +14,10 @@ const signUpUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
-    res.status(200).json(user);
+    //create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
