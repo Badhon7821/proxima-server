189e09007dc06c49b18e23674d0fc1bbbcfb847b
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-03-03 01:16:53 -0800

    user login controller added

--- controllers/UserControler.js
+++ controllers/UserControler.js
@@ -6,7 +6,18 @@ const createToken = (id) => {
 };
 
 const loginUser = async (req, res) => {
-  res.json({ message: "login successfully" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    //create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 const signUpUser = async (req, res) => {
   const { email, password } = req.body;
--- models/UserModel.js
+++ models/UserModel.js
@@ -46,4 +46,24 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All field must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Incorrect Email...");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect Password...");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
